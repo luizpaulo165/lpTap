0b27ffd1f1b92cb3838e7dbd2da0ef568a0252e9
Luiz Paulo de Lima Correia Valença <luizpaulo165@gmail.com>
2011-05-13 22:46:13 -0300

    plugin lpTap version 0.0.1

---
+++ function.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+	 $('#wrap a').lpTab();
+});
---
+++ script/lpTap.js
@@ -0,0 +1,27 @@
+$.fn.lpTab = function(){
+	var openButton = $('.open');
+	var closeButton = $('.close');
+	var wrapContent = $('div#wrap_content')
+	var tabRight = $('div#tip_right')
+	
+	
+	openButton.click(function(){
+		$(function(){
+			wrapContent.fadeIn('fast').animate({left:'0px'},800);	
+		});
+		$(function(){
+			tabRight.stop(true,true).delay(1000).fadeIn('fast').animate({right:'0px'},500);	
+		});
+	});
+	closeButton.click(function(){
+		//$(this).fadeOut('fast');
+		$(function(){
+			wrapContent.stop(true,true).delay(500).animate({left:'-2000px'},1000).fadeOut(10);	
+		});
+		$(function(){
+			tabRight.animate({right:'420px'},500).fadeOut('fast');	
+		});
+	});
+};
+
+
