0ba91c018afd97dbe3559803cec1eaffbfa96b54
Luiz Paulo de Lima Correia Valença <luizpaulo165@gmail.com>
2011-05-16 13:21:00 -0300

    commit update delete function.js

--- function.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function(){
-	 $('#wrap a').lpTab();
-});
--- script/lpTap.js
+++ script/lpTap.js
@@ -7,22 +7,14 @@ $.fn.lpTab = function(){
 	var tabRight = $('#tip_right');
 		
 	openButton.click(function(){
-		$(function(){
-			wrapContent.fadeIn('fast').animate({left:'0px'},800);	
-		});
-		$(function(){
-			tabRight.stop(true,true).delay(1000).fadeIn('fast').animate({right:'0px'},500);	
-		});
-		divFantasma.html(contentDiv);			
+		wrapContent.fadeIn('fast').animate({left:'0px'},800);	
+		tabRight.stop(true,true).delay(1000).fadeIn('fast').animate({right:'0px'},500);	
+		divFantasma.html(contentDiv);		
 	});
 	closeButton.click(function(){
 		//$(this).fadeOut('fast');
-		$(function(){
-			wrapContent.stop(true,true).delay(500).animate({left:'-2000px'},1000).fadeOut(10);	
-		});
-		$(function(){
-			tabRight.animate({right:'420px'},500).fadeOut('fast');	
-		});
+		wrapContent.stop(true,true).delay(500).animate({left:'-2000px'},1000).fadeOut(10);	
+		tabRight.animate({right:'420px'},500).fadeOut('fast');	
 	});
 };
 
