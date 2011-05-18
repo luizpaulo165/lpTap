41ec7c0e93079580cf3c52a942f5decbd4380230
Luiz Paulo de Lima Correia Valença <luizpaulo165@gmail.com>
2011-05-18 12:51:20 -0300

    ajust lpTap.css

--- script/lpTap.js
+++ script/lpTap.js
@@ -5,10 +5,10 @@ $.fn.lpTab = function(options){
 	var wrapRight = "<section id='tip_right'><a href='javascript:void(0)' class='close'>X</a></section>";
 	
 	openButton.click(function(){
-		wrapContent.fadeIn('fast').animate({left:'0px'},800);		
+		wrapContent.fadeIn('fast').animate({left:'0px'},1500);		
 		wrapContent.after(wrapRight);
 		var tabRight = $('#tip_right');			
-		tabRight.stop(true,true).delay(1000).fadeIn('fast').animate({right:'0px'},500);			
+		tabRight.stop(true,true).delay(1500).fadeIn('fast').animate({right:'0px'},500);			
 	});
 	closeButton.live('click',function(){
 		wrapContent.stop(true,true).delay(500).animate({left:'-2000px'},1000).fadeOut(10);
