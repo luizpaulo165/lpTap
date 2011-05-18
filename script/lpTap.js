1f47069a2eecdada48399f5b69c5a8705d8e7d7d
Luiz Paulo de Lima Correia Valença <luizpaulo165@gmail.com>
2011-05-18 09:59:55 -0300

    update lpTap.js

--- script/lpTap.js
+++ script/lpTap.js
@@ -1,21 +1,18 @@
-$.fn.lpTab = function(){
+$.fn.lpTab = function(options){
 	var openButton = $('.open');
 	var closeButton = $('.close');
-	var wrapContent = $('#wrap_lpTap');
-	var wrapRight = $("<section id='tip_right'><a href='javascript:void(0)' class='close'>X</a></section>");
-	var tabRight = $('#tip_right');
+	var wrapContent = $('#' + this.attr('rel'));
+	var wrapRight = "<section id='tip_right'><a href='javascript:void(0)' class='close'>X</a></section>";
 	
 	openButton.click(function(){
-		wrapRight.live('click', function(){
-			$(this).after(wrapContent);
-		});		
+		wrapContent.fadeIn('fast').animate({left:'0px'},800);		
 		wrapContent.after(wrapRight);
-		wrapContent.fadeIn('fast').animate({left:'0px'},800);	
+		var tabRight = $('#tip_right');			
 		tabRight.stop(true,true).delay(1000).fadeIn('fast').animate({right:'0px'},500);			
 	});
-	closeButton.click(function(){
-		//$(this).fadeOut('fast');
-		wrapContent.stop(true,true).delay(500).animate({left:'-2000px'},1000).fadeOut(10);	
+	closeButton.live('click',function(){
+		wrapContent.stop(true,true).delay(500).animate({left:'-2000px'},1000).fadeOut(10);
+		var tabRight = $('#tip_right');	
 		tabRight.animate({right:'420px'},500).fadeOut('fast');	
 	});
 };
